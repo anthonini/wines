320b76716df7252b3a141d3fdb34c0c5fc7f7cae
Anthonini <anthonini@gmail.com>
2018-02-15 21:13:08 -0300

    Initial project commit
    Signed-off-by: Anthonini <anthonini@gmail.com>

---
+++ wines/src/main/java/com/anthonini/wines/WinesApplication.java
@@ -0,0 +1,12 @@
+package com.anthonini.wines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinesApplication.class, args);
+	}
+}
