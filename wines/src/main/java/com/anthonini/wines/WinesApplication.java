870804d81f0d38f1edd2f86528876b1cfc7de35d
Anthonini <anthonini@gmail.com>
2018-02-17 21:31:02 -0300

    Version 1.0.0
    Configured PostgreSQL.
    Updated POM.xml.
    Configured root URL.
    
    
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- wines/src/main/java/com/anthonini/wines/WinesApplication.java
+++ wines/src/main/java/com/anthonini/wines/WinesApplication.java
@@ -6,7 +6,10 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -19,6 +22,16 @@ public class WinesApplication {
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/wines");	
+		}
+		
 	}
 	
 }
